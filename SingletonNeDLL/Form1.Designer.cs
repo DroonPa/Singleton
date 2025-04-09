4535a4337032de0fd1b76f7cce0d5dc066de9bb4
DroonPa <selentiy.denchikk@gmail.com>
2025-04-09 21:41:24 +0300

    Add files via upload

---
+++ SingletonNeDLL/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace SingletonNeDLL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btnCheckDb = new System.Windows.Forms.Button();
+            this.btnSaveDoc = new System.Windows.Forms.Button();
+            this.btnOpenSecondForm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCheckDb
+            // 
+            this.btnCheckDb.Location = new System.Drawing.Point(124, 212);
+            this.btnCheckDb.Name = "btnCheckDb";
+            this.btnCheckDb.Size = new System.Drawing.Size(75, 23);
+            this.btnCheckDb.TabIndex = 5;
+            this.btnCheckDb.Text = "провірка дб";
+            this.btnCheckDb.UseVisualStyleBackColor = true;
+            this.btnCheckDb.Click += new System.EventHandler(this.btnCheckDb_Click);
+            // 
+            // btnSaveDoc
+            // 
+            this.btnSaveDoc.Location = new System.Drawing.Point(124, 183);
+            this.btnSaveDoc.Name = "btnSaveDoc";
+            this.btnSaveDoc.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveDoc.TabIndex = 4;
+            this.btnSaveDoc.Text = "сейв";
+            this.btnSaveDoc.UseVisualStyleBackColor = true;
+            this.btnSaveDoc.Click += new System.EventHandler(this.btnSaveDoc_Click);
+            // 
+            // btnOpenSecondForm
+            // 
+            this.btnOpenSecondForm.Location = new System.Drawing.Point(124, 154);
+            this.btnOpenSecondForm.Name = "btnOpenSecondForm";
+            this.btnOpenSecondForm.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenSecondForm.TabIndex = 3;
+            this.btnOpenSecondForm.Text = "2 форма";
+            this.btnOpenSecondForm.UseVisualStyleBackColor = true;
+            this.btnOpenSecondForm.Click += new System.EventHandler(this.btnOpenSecondForm_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCheckDb);
+            this.Controls.Add(this.btnSaveDoc);
+            this.Controls.Add(this.btnOpenSecondForm);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btnCheckDb;
+        private System.Windows.Forms.Button btnSaveDoc;
+        private System.Windows.Forms.Button btnOpenSecondForm;
+    }
+}
+
---
+++ SingletonNeDLL/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySingletonLibrary;
+
+namespace SingletonNeDLL
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Logger.Instance.Log("Мейн форма запущена");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOpenSecondForm_Click(object sender, EventArgs e)
+        {
+            Logger.Instance.Log("Друга форма відкрита");
+            new SecondForm().Show();
+        }
+
+        private void btnSaveDoc_Click(object sender, EventArgs e)
+        {
+            var result = DocumentSaver.Instance.SaveDocument("Документ збережено!");
+            MessageBox.Show(result);
+            Logger.Instance.Log("Документ збережено");
+        }
+
+        private void btnCheckDb_Click(object sender, EventArgs e)
+        {
+            var result = DbManager.Instance.GetConnectionStatus();
+            MessageBox.Show(result);
+            Logger.Instance.Log("Провірка з'єднання");
+        }
+    }
+}
